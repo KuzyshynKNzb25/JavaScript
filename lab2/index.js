c0284388cf788c786341fcf5831dea58552bdca6
KuzyshynKNzb25 <kuzyshyn.dmytro@comp-sc.if.ua>
2025-03-04 20:55:37 +0200

    Add files via upload

---
+++ lab2/index.js
@@ -0,0 +1,278 @@
+let car1 = new Object();
+car1.color = "yellow";
+car1.maxSpeed = 100;
+driver = new Object();
+driver.name = "Кузишин Дмитро";
+driver.category = "C";
+limitations = "personal limitations";
+accidents_number = "number of accidents"
+
+driver[limitations] = "No driving at night";
+car1.driver = driver;
+car1.tuning = true;
+car1[accidents_number] = 0;
+
+let car2 = {
+    color : "black",
+    maxSpeed : 70,
+    driver:{
+        name : "Кузишин Дмитро", 
+        category : "B",
+        "personal limitations" : null
+    },
+    tuning: false, 
+    "number of accidents":2, 
+    drive: function(){
+        console.log("I can drive anytime");
+    },
+};
+
+car1.drive = function(){
+    console.log("I am not driving at night  .");
+};
+
+class Truck{
+    constructor(color, weight, avgSpeed, brand, model){
+        this.color = color;
+        this.weight = weight;
+        this.avgSpeed = avgSpeed;
+        this.brand = brand;
+        this.model = model;
+        this.trip = function(){
+            if(!driver){
+                console.log("No driver assigned");
+                return;
+            }
+    
+            console.log("Driver" + this.driver);
+            if(this.driver.nightDriving){
+                console.log("drives at night");
+            }
+            else {
+                console.log("does not drive at night");
+            }
+            console.log("and has " + this.driver.experience + " years of experience");
+        }
+    }
+
+}
+    Truck.prototype.AssignDriver = function(name, nightDriving, experience) {
+        this.driver = {
+            name: name,
+            nightDriving: nightDriving,
+            experience: experience
+        };
+    }; 
+
+class Square{
+    constructor(a){
+        this.a = a;
+    }
+    
+
+    length(){
+        if(!this.a){
+            console.log("sides are underfined");
+            return;
+        }
+        console.log(`Sum of all sides: ${this.length()}`);
+    }
+
+    square(){
+        if(!this.a){
+            console.log("side a is underfined");
+            return;
+        }
+        console.log(`Square: ${this.square()}`);
+    }
+
+    info(){
+        console.log(`Side a: ${this.a}\nSide b: ${this.a}\nSide c: ${this.a}\nSide d: ${this.a}`);
+        console.log("Angle a:90\nAngle b:90\nAngle c:90\nAngle d:90\n");
+        this.length();
+        this.square();
+    }
+
+    static help = function(){
+        console.log("It's a square, that figure has all sides equal");
+    }
+
+}
+
+class Rectangle extends Square{ 
+    constructor(a, b){
+        super(a);
+        this.b = b;
+    }
+
+    static help = function(){
+        console.log("It's a rectangle, that figure has all angles equal to 90 degrees");
+    }
+
+
+
+    length(){
+        if(!this.a || !this.b){
+            console.log("sides are underfined");
+            return;
+        }
+        console.log(`Sum of all sides: ${this.a *2 +this.b*2} `);
+    }
+
+    square(){
+        if(!this.a || !this.b){
+            console.log("side a or b is underfined");
+            return;
+        }
+        console.log(`Square: ${this.a*this.b}`) ;
+    }
+
+    info(){
+        console.log(`Side a: ${this.a}\nSide b: ${this.b}\nSide c: ${this.a}\nSide d: ${this.b}`);
+        console.log("Angle a:90\nAngle b:90\nAngle c:90\nAngle d:90\n");
+        this.length();
+        this.square();
+    }
+}
+
+class Rhombus extends Square{
+    constructor(a, alpha, beta){
+        super(a);
+        this.alpha = alpha;
+        this.beta = beta;
+    }
+
+    static help = function(){
+        console.log("It's a rhombus, that figure has all sides equal");
+    }
+
+    length(){
+        if(!this.a){
+            console.log("sides are underfined");
+            return;
+        }
+        console.log(`Sum of all sides:${this.a*4}`);
+        
+    }
+
+    #localCounter(){
+        let halfOfAlpha = this.alpha*Math.PI/360;
+        let halfOfDiameter1 = this.a*Math.sin(halfOfAlpha);
+        let halfOfDiameter2 = Math.sqrt(this.a*this.a-halfOfDiameter1*halfOfDiameter1);
+        return 2*halfOfDiameter1*halfOfDiameter2;
+    }
+
+    square(){
+        if(!this.a || !this.alpha || !this.beta){
+            console.log("side a or angles are underfined");
+            return;
+        }        
+        console.log(`Square: ${this.#localCounter()}`);        
+    }
+
+    info(){
+        console.log(`Side a: ${this.a}\nSide b: ${this.a}\nSide c: ${this.a}\nSide d: ${this.a}` );
+        console.log(`Angle a:${this.alpha}\nAngle b:${this.beta}\nAngle c:${this.alpha}\nAngle d:${this.beta}\n`);
+        this.length();
+        this.square();
+    }
+
+    get a(){
+        return this._a;
+    }
+
+    get alpha(){
+        return this._alpha;
+    }
+
+    get beta(){
+        return this._beta;
+    }
+
+    set a(sideSize){
+        this._a = sideSize;
+    }
+
+    set alpha(angleA){
+        this._alpha = angleA;
+    }
+
+    set beta(angleB){
+        this._beta = angleB;
+    }
+}
+
+class Parallelogram extends Rectangle{
+    constructor(a, b, alpha, beta){
+        super(a, b);
+        this.alpha = alpha;
+        this.beta = beta;
+    }
+
+    static help = function(){
+        console.log("It's a rhombus, that figure has all sides and angles different ");
+    }
+
+    length(){
+        if(!this.a || !this.b){
+            console.log("sides are underfined");
+            return;
+        }
+        console.log(`Sum of all sides:${this.a*2+this.b*2}`)        
+    }
+
+    #localCounter(){
+        if(this.beta > 90){
+            this.beta -=90;
+        }
+
+        const height = this.a * Math.sin(beta);
+        return height*this.a;
+    }
+
+    square(){
+        if(!this.a || !this.alpha || !this.beta || !this.b){
+            console.log("sides a or angles are underfined");
+            return;
+        }        
+        console.log(`Square: ${this.#localCounter()}`);        
+    }
+
+    info(){
+        console.log(`Side a: ${this.a}\nSide b: ${this.b}\nSide c: ${this.a}\nSide d: ${this.b}` );
+        console.log(`Angle a:${this.alpha}\nAngle b:${this.beta}\nAngle c:${this.alpha}\nAngle d:${this.beta}\n`);
+        this.length();
+        this.square();
+    }
+}
+
+function Triangular(a = 3, b = 4, c = 5){
+    return {a, b, c};
+}
+
+function PiMultiplier(num){
+    const result = function(num){
+        return Math.PI * num;
+    }
+    return result(num);
+}
+
+function Painter(color) {
+    return function (obj) {
+        if (obj.type) {
+            console.log(`Color: ${color}, Type: ${obj.type}`);
+        } else {
+            console.log("No ‘type’ property occurred!");
+        }
+    };
+}
+
+function Painter(color) {
+    return function (obj) {
+        if (obj.type) {
+            console.log(`color: ${color}\n type: ${obj.type}`);
+        } else {
+            console.log("No 'type' property occurred!");
+        }
+    };
+}
