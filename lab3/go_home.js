12755f5f48bb6a24350c90cfb875f73511ab5c44
KuzyshynKNzb25 <kuzyshyn.dmytro@comp-sc.if.ua>
2025-03-11 14:46:58 +0200

    Update go_home.js

--- lab3/go_home.js
+++ lab3/go_home.js
@@ -1,6 +1,6 @@
 (function(window){
     const goHome = {};
-    let speakWord = "why are you still not at home?";
+    let speakWord = "why do you still not at home?";
     goHome.speak = function (name) {
         console.log(name  + " " + speakWord);
       }
